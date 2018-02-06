af106de5ab5c0b4bfde1d91c8eb8804a787ab17d
Avinash Ranjan <avinash.ranjan21@gmail.com>
2018-02-06 14:55:27 +0530

    Initial commit

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="3befe1b0-bc32-454a-bae6-9b85b950d220")
+public class DataBindingInfo {}
